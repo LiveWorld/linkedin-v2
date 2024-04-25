60b351a12550188352fda2fc1133bd805f9cdfb0
Daniel Lam <dlam@liveworld.com>
2024-04-25 09:03:51 -0400

    remove debugging

--- lib/linked_in/ads.rb
+++ lib/linked_in/ads.rb
@@ -3,7 +3,7 @@ module LinkedIn
   class Ads < APIResource
 
     # This requires the URN to be encoded, so we need to use the protocol v2 connection
-    # this is ok until       @api_version = "202307"
+    # this is ok until @api_version = "202307"
     def ad_direct_sponsored_contents(options = {})
       urn = options.delete(:urn)
       path = "/adDirectSponsoredContents?owner=#{CGI::escape(urn)}&q=owner"
--- lib/linked_in/images.rb
+++ lib/linked_in/images.rb
@@ -4,8 +4,6 @@ module LinkedIn
 
     def batched_images(options={})
       image_urns = options.delete(:image_urns)
-      # formatted_person_ids = person_ids.map{|id| "(id:#{id})"}.join(',')
-      p "urns: #{image_urns}"
       get("/images?ids=List(#{image_urns})", options)
     end
 
